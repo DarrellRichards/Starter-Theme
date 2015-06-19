342c0e99687827752ff09401b0f079386f0d602f
Darrell Richards <hello@darrellrichards.com>
2015-06-19 10:54:44 -0400

    Updated Basic Layout of theme added support for posts thumbnails

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 	$context = Timber::get_context();
 	$context['posts'] = Timber::get_posts();
-	$about['post'] = new TimberPost(8);
-	Timber::render('index.twig', $context, $about);
+	Timber::render('index.twig', $context);
 ?>
 
